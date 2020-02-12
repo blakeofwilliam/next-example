8f0008ca6b5dcb3da06bd82d843ca90ec2d287c4
Blake Williams <blakethewilliams@gmail.com>
2020-02-11 21:30:43 -0800

    initial commit

---
+++ components/AnotherComponent.js
@@ -0,0 +1,5 @@
+const AnotherComponent = () => (
+  <p>And it uses another component!</p>
+)
+
+export default AnotherComponent
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import AnotherComponent from '../components/AnotherComponent'
+
+const HomePage = () => (
+  <div>
+    <h1>This is a Next.js app!</h1>
+    <AnotherComponent />
+  </div>
+)
+
+export default HomePage
